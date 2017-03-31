b92b9d3bb88eea53b5b40f47c3f3b0a92a9ef2bb
Eric Collazo <ecollazo@consultant.com>
2017-03-31 16:21:50 -0400

    Added components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TopContainerComponent } from './top-container/top-container.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartComponent } from './chart/chart.component';
+import { StockListComponent } from './stock-list/stock-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopContainerComponent,
+    SidebarMenuComponent,
+    DashboardComponent,
+    ChartComponent,
+    StockListComponent
   ],
   imports: [
     BrowserModule,
